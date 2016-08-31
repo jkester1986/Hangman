6c668ad84bfa7cd151d0ae60aa8f327a08bbfeb6
Julia Kester <Julia Kester>
2016-08-31 15:07:45 -0400

    display rand word

--- hangman.js
+++ hangman.js
@@ -1,4 +1,35 @@
 // JavaScript Document
+var api = 'http://api.wordnik.com/v4/words.json/randomWord';
+var key = 'a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
+
+var corpus = 2000; // higher = less complex words
+var def = true; // only words with definitions ?
+
+function getWord(type, c) {
+    return $.get(
+        api + 
+        '?includePartOfSpeech=' + type +
+        '&hasDictionaryDef=' + def +
+        '&minCorpusCount=' + corpus + 
+        '&api_key=' + key
+    );    
+}
+
+function generateWord(){
+	var word = "";
+	$.when (getWord('noun'))
+		.then(function(genWord){
+			word = genWord.word;
+			console.log(word);
+			var wordDiv = document.getElementById("word");
+			wordDiv.innerHTML = word;
+		})
+		.fail(function() { alert('ERROR'); });
+	;
+	
+	return word;
+}
+
 function guess(letter){
 	//if letter is right
 	
